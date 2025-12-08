40665e3d9b8b05fd96a91bcb3593fa6535215e1d
Mebi <109687165+mebiae@users.noreply.github.com>
2025-12-07 19:13:14 -0500

    Add files via upload

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+﻿module.exports = function(eleventyConfig) {
+
+    // De-index.html the output
+    eleventyConfig.addGlobalData("permalink", () => {
+      return (data) => `${data.page.filePathStem}.${data.page.outputFileExtension}`;
+    });
+
+    eleventyConfig.addPassthroughCopy("./src/assets");
+    eleventyConfig.addPassthroughCopy("./src/css");
+    eleventyConfig.addPassthroughCopy("./src/js");
+
+    return {
+      passthroughFileCopy: true,
+      htmlTemplateEngine: "njk",
+      markdownTemplateEngine: "njk",
+      dir: {
+        input: "src",
+        output: "public",
+        includes: "_includes",
+      },
+    };
+}
